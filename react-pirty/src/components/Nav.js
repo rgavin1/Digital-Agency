8b8c449d1dc07af13b0b6c6a0b62e01323d83434
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-12-31 22:08:39 -0500

    Completed Navigation w/o menu

--- react-pirty/src/components/Nav.js
+++ react-pirty/src/components/Nav.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Nav = () => {
-    return (
-        <div className="nav-container">
-            <span>PRITY</span>
-            <div>
-                <span>Work</span>
-                <span>News</span>
-                <span>About</span>
-                <span>Contact</span>
+class Nav extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            toggled: true 
+        }
+        this.handleMenu = this.handleMenu.bind(this);
+    }
+
+    handleMenu = () => {
+        this.setState({
+            toggled: !this.state.toggled
+        });
+    }
+    
+    render(){
+        return (
+            <div className="nav-container">
+                <span className="brand-name">PRITY</span>
+                <div className="nav-list">
+                    <span>Work</span>
+                    <span>News</span>
+                    <span>About</span>
+                    <span>Contact</span>
+                </div>
+                <FontAwesomeIcon
+                className="toggle-menu"
+                onClick={this.handleMenu} 
+                icon={ this.state.toggled ? faBars : faTimes } 
+                />
+                <div className={ this.state.toggled ? 'closed' : 'open'} ></div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Nav;
